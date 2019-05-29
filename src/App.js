f035884f3867d1e11ce1c0d1e58c4dcfac334bba
krikaz <pascal.bou@protonmail.com>
2019-05-29 14:11:11 +0200

    created list render, new task input, add task button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,74 @@
 import React from 'react';
 
+const initialData = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+      toDoList: initialData,
+      task: 'start here',
+    };
+  }
+
+  changeHandler = event => {
+    this.setState({
+      task: event.target.value
+    });
+  };
+
+  addTask = () => {
+    const newtask = {
+      task: this.state.task,
+      id: Date.now(),
+      completed: false,
+    };
+
+    this.setState({
+      toDoList: this.state.toDoList.concat(newtask),
+      task: ''
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To Do List</h2>
+        {this.state.toDoList.map(taskObj => (
+          <div key={taskObj.id}>{taskObj.task}</div>
+          // <div>{taskObj.task}</div>
+        ))}
+        <TaskAdder
+          task={this.state.task}
+          changeHandler={this.changeHandler}
+          addTask={this.addTask}
+        />
       </div>
     );
   }
+}
+
+function TaskAdder({ task, changeHandler, addTask }) {
+  return (
+    <div>
+      <input value={task} onChange={changeHandler} type="text" />
+      <button onClick={addTask}>Add Task</button>
+    </div>
+  );
 }
 
 export default App;
